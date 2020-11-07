48362cb7225245b3bf36b20ca9abe03db318c486
remyrascal <37592530+remyrascal@users.noreply.github.com>
2020-11-07 12:24:36 -0500

    more remaining vote data

---
+++ interfaces.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NYT_API = void 0;
+var NYT_API;
+(function (NYT_API) {
+    ;
+    ;
+    ;
+})(NYT_API = exports.NYT_API || (exports.NYT_API = {}));
+;
